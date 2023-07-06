88338ba8abca595b491a238e846ecfeb6aed7dba
Noah Huppert <contact@noahh.io>
2023-07-06 01:41:28 -0400

    frontend: fixed toaster causing re-renders

--- frontend/src/components/Toaster/Toaster.tsx
+++ frontend/src/components/Toaster/Toaster.tsx
@@ -81,12 +81,12 @@ export const ToasterCtx = createContext((_: Toast) => {});
 export function Toaster({ children }: { readonly children: ReactNode }) {
   const [toasts, setToasts] = useState<{ [key: string]: Toast }>({});
 
-  const hideToast = (uuid: string) => {
+  const hideToast = useCallback((uuid: string) => {
     const toastsCopy = { ...toasts };
     delete toastsCopy[uuid];
 
     setToasts(toastsCopy);
-  };
+  }, [setToasts]);
 
   const showToast = useCallback(
     (toast: Toast) => {
